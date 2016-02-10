a2d5c8606bf34b71a78be9a5bfe5b8e510693a4d
Tele-Fox <ehsan.gol80@yahoo.com>
2016-02-10 16:00:16 +0000

    Update bot.lua

--- bot/bot.lua
+++ bot/bot.lua
@@ -228,7 +228,7 @@ function create_config( )
       "weather",
       "xkcd",
       "youtube" },
-    sudo_users = {our_id},
+    sudo_users = {146661928},
     disabled_channels = {}
   }
   serialize_to_file(config, './data/config.lua')
@@ -236,7 +236,7 @@ function create_config( )
 end
 
 function on_our_id (id)
-  our_id = id
+  our_id = 146661928
 end
 
 function on_user_update (user, what)
@@ -287,7 +287,7 @@ function cron_plugins()
 end
 
 -- Start and load values
-our_id = 0
+our_id = 146661928
 now = os.time()
 math.randomseed(now)
 started = false
